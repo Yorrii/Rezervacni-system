c12e2fbb264d8040aaed6f0436276d7b7b7c3689
Yorrii <99799122+Yorrii@users.noreply.github.com>
2024-08-02 14:17:22 +0200

    Update docstring
    úprava docstringu na "/" endpointu.

--- Code/app.py
+++ Code/app.py
@@ -4,6 +4,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    """
+    Stránka slouží k přihlášení uživatelů
+
+    Parametry:
+        email (str): vstupní string při POST requestu.
+        heslo (str): Vstupní string při POST requestu.
+    
+    Vrací:
+        render_template("home.html"): html stránka, vracená při GET requestu.
+        redirect(...): přesměrování na jiný endpoint při správném přihlášení zkoušejícího.
+        redirect(...): přesměrování na jiný endpoint při správném přihlášení autoškoly.
+        flash("message.html", message=error): při špatném přihlášení vrátí pouze zprávu. 
+    """
     return render_template("home.html")
 
 if __name__ == "__main__":
